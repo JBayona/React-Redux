4a7471fff934ace321a817a39cb4ee2f66c28d8e
JBayona <jordavids_22@hotmail.com>
2017-07-06 13:05:23 -0500

    Add a simple test to verify ReactJS render

--- learn-redux/client/reduxstagram.js
+++ learn-redux/client/reduxstagram.js
@@ -1,1 +1,12 @@
 // let's go!
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+//Import CSS, webpack se encargará de importar el css
+import css from './styles/style.styl';
+
+ReactDOM.render(<p>Hi</p>, document.getElementById('root'));
+
+
+
+
