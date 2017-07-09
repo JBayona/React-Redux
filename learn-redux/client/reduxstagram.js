d7482f265a5813b3997afa960f4d35ddb5a5c4b1
JBayona <jordavids_22@hotmail.com>
2017-07-09 14:10:06 -0500

    Integrate our Store with ReactJS

--- learn-redux/client/reduxstagram.js
+++ learn-redux/client/reduxstagram.js
@@ -12,14 +12,19 @@ import PhotoGrid from './components/PhotoGrid';
 
 //Import React router devs
 import {Router, Route, IndexRoute, browserHistory} from 'react-router';
+import {Provider} from 'react-redux';
+import store, {history} from './store';
 
 const router = (
-	<Router history = {browserHistory}>
-		<Route path ="/" component = {Main}>
-			<IndexRoute component = {PhotoGrid}></IndexRoute>
-			<Route path ="/view/:postId" component = {Single}></Route>
-		</Route>
-	</Router>
+	/*Expone el store en la app*/
+	<Provider store = {store}>
+		<Router history = {history}>
+			<Route path ="/" component = {Main}>
+				<IndexRoute component = {PhotoGrid}></IndexRoute>
+				<Route path ="/view/:postId" component = {Single}></Route>
+			</Route>
+		</Router>
+	</Provider>
 )
 
 
